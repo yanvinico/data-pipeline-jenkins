068f533df1501553e00117d58663597a4291e96e
Yan Vinicius <yanviniciusgb@outlook.com>
2022-11-02 01:50:18 -0300

    commit adicionando senha de root

--- Tema9/Script/sh/enviroment.sh
+++ Tema9/Script/sh/enviroment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Iamthemaster99" | sudo -S sleep 1 && sudo su
 WORKSPACE=$1
 virtualenv env
 source $WORKSPACE/env/bin/activate
--- Tema9/Script/sh/start.sh
+++ Tema9/Script/sh/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Iamthemaster99" | sudo -S sleep 1 && sudo su
 WORKSPACE=$1
 source $WORKSPACE/env/bin/activate
 python3 $WORKSPACE/Script/main.py
