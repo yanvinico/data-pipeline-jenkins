1271ded7e2b53069f657f247db6973c5218740e5
Yan Vinicius <yanviniciusgb@outlook.com>
2022-11-03 15:25:01 -0300

    tentando mudar o workspace

--- Tema9/Script/sh/start.sh
+++ Tema9/Script/sh/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 WORKSPACE=$1
-source $WORKSPACE/env/bin/activate
-python3 $WORKSPACE/Script/main.py
+source /var/lib/jenkins/workspace/teste-jenkins@script/bdbb79856b515f1affed826149d3ef073e97ec0b0b7b41b209bebdc9b5e457e5/env/bin/activate
+python3 /var/lib/jenkins/workspace/teste-jenkins@script/bdbb79856b515f1affed826149d3ef073e97ec0b0b7b41b209bebdc9b5e457e5/Script/main.py
