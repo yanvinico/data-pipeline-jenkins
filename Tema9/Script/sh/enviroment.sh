0b90ae7cff230e2e99a9f5ea66281e64014e9928
Yan Vinicius <yanviniciusgb@outlook.com>
2022-11-03 15:30:57 -0300

    corrigindo nomes dos arquivos

--- Tema9/Script/sh/enviroment.sh
+++ Tema9/Script/sh/enviroment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 WORKSPACE=$1
 virtualenv env
-source /var/lib/jenkins/workspace/teste-jenkins@script/bdbb79856b515f1affed826149d3ef073e97ec0b0b7b41b209bebdc9b5e457e5/env/bin/activate
-command pip install -r /var/lib/jenkins/workspace/teste-jenkins@script/bdbb79856b515f1affed826149d3ef073e97ec0b0b7b41b209bebdc9b5e457e5/Script/requirements.txt
+source $WORKSPACE/env/bin/activate
+command pip install -r $WORKSPACE/Script/requirements.txt
--- Tema9/Script/sh/sync.sh
+++ Tema9/Script/sh/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 WORKSPACE=$1
-aws s3 sync "/var/lib/jenkins/workspace/teste-jenkins@script/bdbb79856b515f1affed826149d3ef073e97ec0b0b7b41b209bebdc9b5e457e5/Script/result" "s3://jt-dataeng-yanvinicius/tema9/result"
+aws s3 sync "$WORKSPACE/Script/result" "s3://jt-dataeng-yanvinicius/tema9/result"
