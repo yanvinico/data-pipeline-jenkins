c9f169e6784f444b23ba84785fdcef22ff2d7052
Yan Vinicius <yanviniciusgb@outlook.com>
2022-11-03 15:57:52 -0300

    Update sync.sh

--- Tema9/Script/sh/sync.sh
+++ Tema9/Script/sh/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 WORKSPACE=$1
-aws s3 sync "/var/lib/jenkins/workspace/teste-jenkins/Tema9/Script/result" "s3://jt-dataeng-yanvinicius/tema9/result"
+aws s3 sync "/var/lib/jenkins/workspace/teste-jenkins/result" "s3://jt-dataeng-yanvinicius/tema9/result"
