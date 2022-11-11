785a5b7ee373f36251ec2144256f42326f12123b
Yan Vinicius <yanviniciusgb@outlook.com>
2022-11-11 15:28:18 -0300

    ponto no lugar do pwd

--- Tema9/Script/sh/start.sh
+++ Tema9/Script/sh/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 WORKSPACE=$1
-source $WORKSPACE/env/bin/activate
-python3 $WORKSPACE/Tema9/Script/main.py
+source ./env/bin/activate
+python3 ./Tema9/Script/main.py
--- Tema9/Script/sh/sync.sh
+++ Tema9/Script/sh/sync.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-WORKSPACE=$(PWD)
-aws s3 sync "$WORKSPACE/result" "s3://jt-dataeng-yanvinicius/tema9/result"
+aws s3 sync "./result" "s3://jt-dataeng-yanvinicius/tema9/result"
