20343575161e9f1841a3a381c944d918e300a488
Yan Vinicius <yanviniciusgb@outlook.com>
2022-11-11 18:15:25 -0300

    detalhes

--- Tema10/Script/sh/container.sh
+++ Tema10/Script/sh/container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -t --name container-tema10 -v ./Tema10/result:/Tema10/result Tema10
+docker run -t --name container-tema10 -v ./Tema10/result:/Tema10/result tema10
--- Tema10/Script/sh/image.sh
+++ Tema10/Script/sh/image.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t Tema10 ./Tema10
+docker build -t tema10 ./Tema10
