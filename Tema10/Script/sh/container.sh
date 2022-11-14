1919d46dea261cf294e7f9dc5d82873a0b338073
Yan Vinicius <yanviniciusgb@outlook.com>
2022-11-14 15:53:39 -0300

    Update container.sh

--- Tema10/Script/sh/container.sh
+++ Tema10/Script/sh/container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker run -t --name container-tema10 tema10
-docker cp container_tema10:/Tema10/result/ ./finalResults/
-docker stop container_tema10
+docker cp container-tema10:/Tema10/result/ ./finalResults/
+docker stop container-tema10
